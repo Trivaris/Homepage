c75de6d1b29451a4cf2514a171c699453fc6c739
trivaris <github@tripple.lurdane.de>
2025-08-19 23:44:57 +0200

    feat: Split VSCodium Devshell

--- flake.nix
+++ flake.nix
@@ -15,30 +15,41 @@
       pkgs.vscode-extensions.jnoortheen.nix-ide
     ];
   in {
-    devShells."x86_64-linux".default = pkgs.mkShell {
-      buildInputs = (builtins.attrValues {
-        inherit (pkgs)
-          nodejs_22
-          pnpm
-          svelte-language-server
-          typescript-language-server
-          tailwindcss-language-server
-          eslint_d
-        ;
-      }) ++ extensions;
-      shellHook = ''
-        EXTDIR=$(mktemp -d)
-        echo "Preparing VSCodium extensions in $EXTDIR"
+    devShells."x86_64-linux" = {
+        default = pkgs.mkShell {
+        buildInputs = builtins.attrValues {
+          inherit (pkgs)
+            nodejs_22
+            pnpm
+          ;
+        };
+      };
+      vscodium = pkgs.mkShell {
+        buildInputs = (builtins.attrValues {
+          inherit (pkgs)
+            nodejs_22
+            pnpm
+            vscodium
+            svelte-language-server
+            typescript-language-server
+            tailwindcss-language-server
+            eslint_d
+          ;
+        }) ++ extensions;
+        shellHook = ''
+          EXTDIR=$(mktemp -d)
+          echo "Preparing VSCodium extensions in $EXTDIR"
 
-        # symlink all nix-provided extensions
-        for e in ${builtins.concatStringsSep " " (map (ext: "${ext}/share/vscode/extensions/*") extensions)}; do
-          ln -s "$e" "$EXTDIR/"
-        done
+          # symlink all nix-provided extensions
+          for e in ${builtins.concatStringsSep " " (map (ext: "${ext}/share/vscode/extensions/*") extensions)}; do
+            ln -s "$e" "$EXTDIR/"
+          done
 
-        # open codium with these extensions
-        echo "Launching VSCodium with extensions from $EXTDIR..."
-        codium --extensions-dir "$EXTDIR" "$PWD" &
-      '';
+          # open codium with these extensions
+          echo "Launching VSCodium with extensions from $EXTDIR..."
+          codium --extensions-dir "$EXTDIR" "$PWD" &
+        '';
+      };
     };
   };
 }
